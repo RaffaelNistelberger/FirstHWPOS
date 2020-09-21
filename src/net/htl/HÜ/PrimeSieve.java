ebbf0da518b33de2116edb114549ead088a55c4f
RaffaelNistelberger <Nistelberger.Raffael@sus.htl-grieskirchen.at>
2020-09-21 15:20:24 +0200

    initial commit

---
+++ src/net/htl/HÜ/PrimeSieve.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.htl.HÜ;
+
+/**
+ *
+ * @author raffa
+ */
+public interface PrimeSieve {
+
+    public boolean isPrime(int p);
+
+    public void printPrimes();
+
+}
